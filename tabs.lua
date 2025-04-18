d81d68b26f0f37890f29c6e6bdba19c01c20a18a
semi556tgb <dredayvon@Gmail.com>
2025-04-18 10:28:09 -0500

    Update tabs.lua

--- tabs.lua
+++ tabs.lua
@@ -1,20 +1,14 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/drillygzzly/Roblox-UI-Libs/main/1%20Tokyo%20Lib%20(FIXED)/Tokyo%20Lib%20Source.lua"))({
-    cheatname = "Your Script Name",
-    gamename = "Your Game Name",
-})
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/drillygzzly/Roblox-UI-Libs/main/1%20Tokyo%20Lib%20(FIXED)/Tokyo%20Lib%20Source.lua"))()
 
-library:init()
-
-local Window = library.NewWindow({
-    title = "Your Script Name | Your Game Name",
-    size = UDim2.new(0, 510, 0.6, 6)
-})
+local Window = Library:CreateWindow("My Script") -- make sure this matches the real library's API
 
 local Tabs = {}
 
-Tabs.Visuals = Window:AddTab("Visuals")
-Tabs.Main = Window:AddTab("Main")
-Tabs.Exploits = Window:AddTab("Exploits")
-Tabs.Settings = library:CreateSettingsTab(Window)
+Tabs.Main = Window:CreateTab("Main")
+Tabs.Visuals = Window:CreateTab("Visuals")
+Tabs.Exploits = Window:CreateTab("Exploits")
+Tabs.Settings = {
+    library = Library
+}
 
 return Tabs
